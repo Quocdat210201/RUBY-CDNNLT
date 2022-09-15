e8c6324b2769ad2516ccc8798966f278fa7c290c
Quocdat210201 <pqd2102@gmail.com>
2022-09-15 23:47:04 +0700

    Creat new

---
+++ Argument/HelloWrold.rb
@@ -0,0 +1,11 @@
+puts "Hello " + "Phan Quốc Đạt " + "1911505310112"
+
+puts "Nhâp số a:"
+a = gets
+puts "Nhâp số b:"
+b = gets
+if a > b
+    puts "Số lớn nhất là : " + a
+else 
+    puts "Số lớn nhất là : " + b
+end
