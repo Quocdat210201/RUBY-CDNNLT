d8473a038f562478f22840230717d41fa685da3d
Quocdat210201 <pqd2102@gmail.com>
2022-09-17 02:57:52 +0700

    Complete Argument #2 17/9

--- Argument3/studentClass.rb
+++ Argument3/studentClass.rb
@@ -1,18 +1,61 @@
-class Students
-    def initialize
-        puts "Chao mung ban den voi Ruby!"
+class SinhVien
+    attr_accessor :name, :id, :mLy, :mHoa, :mToan
+  
+    def initialize(name, id)
+      @name = name
+      @id = id
     end
-    def Name
-        puts "Nhap ten sinh vien :"
-        names = gets
+  
+    def set_name(name)
+      @name = name
     end
-    def Class
-        puts "Nhap lop :"
-        classs = gets
-        puts "aaaa"
+  
+    def get_name
+      @name
     end
-end
-
-student = Students.new
-puts student.Name
-puts student.Class
+  
+    def set_id(id)
+      @id = id
+    end
+  
+    def get_id
+      @id
+    end
+  
+    def nhap_diem_toan(mToan)
+      @mToan = mToan
+    end
+  
+    def nhap_diem_ly(mLy)
+      @mLy = mLy
+    end
+  
+    def nhap_diem_hoa(mHoa)
+      @mHoa = mHoa
+    end
+  
+    def get_dtb
+      (@mToan + mLy + mHoa) / 3.0
+    end
+  
+    def display
+      puts "Tên sinh viên: #{@name}"
+      puts "Mã sinh viên: #{@id}"
+      puts "Điểm trung bình: #{get_dtb().round(2)}"
+    end
+  end
+  system("cls")
+  print "Nhập họ tên sinh viên: "
+  name = gets.chomp
+  print "Nhập mã sinh viên: "
+  id = gets.chomp
+  std = SinhVien.new(name, id)
+  print "Nhập vào điểm hóa: "
+  std.nhap_diem_hoa(gets.chomp.to_i)
+  print "Nhập vào điểm lý: "
+  std.nhap_diem_ly(gets.chomp.to_i)
+  print "Nhập vào điểm toán: "
+  std.nhap_diem_toan(gets.chomp.to_i)
+  puts "\n===\tTHÔNG TIN SINH VIÊN: #{std.get_name}\t==="
+  std.display
+  
